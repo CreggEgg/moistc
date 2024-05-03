797e5eceb0b67492ebf7b69b417281598cd84957
FCSRobotics <mschoifet@friendscentral.org>
2024-05-03 16:16:07 -0400

    started types

--- src/compiler/types.rs
+++ src/compiler/types.rs
@@ -1,4 +1,4 @@
-use crate::parser::Func;
+use crate::parser::{Expr, Func};
 
 pub enum Type {
     Int,
@@ -6,4 +6,25 @@ pub enum Type {
     Bool,
 }
 
-pub fn generate_types(funcs: Vec<Func>) {}
+pub struct FunctionType {
+    params: Vec<Type>,
+    returns: Vec<Type>,
+}
+
+pub struct TypedExpr(Type, Expr);
+pub struct TypedFunc(FunctionType, Func);
+
+pub fn generate_types(funcs: Vec<Func>) -> Vec<TypedExpr> {
+    funcs
+        .iter()
+        .map(|func| {
+            TypedFunc(
+                FunctionType {
+                    params: vec![],
+                    returns: vec![],
+                },
+                *func,
+            )
+        })
+        .collect::<Vec<TypedFunc>>()
+}
